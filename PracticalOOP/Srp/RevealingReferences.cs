ab9cb99cad856b4008ab5b71d0ada8d0b54c509f
tashinoki <kenya.112163@gmail.com>
2022-04-11 21:32:33 +0900

    add revealing reference

---
+++ PracticalOOP/Srp/RevealingReferences.cs
@@ -0,0 +1,26 @@
+﻿namespace PracticalOOP.Srp
+{
+    internal class RevealingReferences
+    {
+        private Wheel[] Wheels { get; set; }
+
+        internal RevealingReferences(int[][] data)
+        {
+            Wheels = Wheelify(data);
+        }
+
+        internal int[] Diameters()
+            => Array.ConvertAll(
+                Wheels,
+                new Converter<Wheel, int>((w) => w.Rim + w.Tire * 2));
+
+        private Wheel[] Wheelify(int[][] data)
+            => Array.ConvertAll(
+                data,
+                new Converter<int[], Wheel>((d => new Wheel(d[0], d[1]))));
+
+        private record Wheel(int Rim, int Tire)
+        {
+        }
+    }
+}
