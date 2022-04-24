c00079a70aa506af00c8a17a04dece537f046385
tashinoki <kenya.112163@gmail.com>
2022-04-24 15:06:35 +0900

    add safety book

---
+++ SecureByDesign/Integrity/SafetyBook.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecureByDesign.Integrity
+{
+    internal class SafetyBook
+    {
+        public SafetyBook(string title, string description, string author)
+        {
+            Title = title;
+            Description = description;
+            Autor = author;
+        }
+
+        public string Title { get; private set; }
+
+        public string Description { get; private set; }
+
+        public string Autor { get; private set; }
+    }
+}
