d31b8972ac610b98c9a8dda95301bc37ea41d896
tashinoki <kenya.112163@gmail.com>
2022-04-24 15:17:10 +0900

    nits

--- SecureByDesign/Integrity/SafetyBook.cs
+++ SecureByDesign/Integrity/SafetyBook.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SecureByDesign.Integrity
+﻿namespace SecureByDesign.Integrity
 {
     internal class SafetyBook
     {
