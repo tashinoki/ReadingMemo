f06acdaa66215dcac64485e1754e33138bdca577
tashinoki <kenya.112163@gmail.com>
2022-04-24 15:06:59 +0900

    use init only

--- SecureByDesign/Integrity/SafetyBook.cs
+++ SecureByDesign/Integrity/SafetyBook.cs
@@ -8,17 +8,10 @@ namespace SecureByDesign.Integrity
 {
     internal class SafetyBook
     {
-        public SafetyBook(string title, string description, string author)
-        {
-            Title = title;
-            Description = description;
-            Autor = author;
-        }
+        public string Title { get; init; }
 
-        public string Title { get; private set; }
+        public string Description { get; init; }
 
-        public string Description { get; private set; }
-
-        public string Autor { get; private set; }
+        public string Autor { get; init; }
     }
 }
