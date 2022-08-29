f6410142654be63474a419bb93ef705b16e66562
tashinoki <kenya.112163@gmail.com>
2022-08-29 20:00:26 +0900

    use inheritance you want to add behavior to a existing class.

---
+++ LegacyCode/DifferentialProgramming/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegacyCode.DifferentialProgramming;
+
+internal class Employee
+{
+    public void Pay()
+    {
+        // hoge
+    }
+}
+
+internal class LoggingEmployee : Employee
+{
+    public new void Pay()
+    {
+        // hogehoge
+        base.Pay();
+    }
+}
