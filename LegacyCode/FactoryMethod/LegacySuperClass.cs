fc3fd70e3e566e5fef8f7452e9e9375ff815d0f3
tashinoki <kenya.112163@gmail.com>
2022-09-01 23:05:33 +0900

    factory method pattern

---
+++ LegacyCode/FactoryMethod/LegacySuperClass.cs
@@ -0,0 +1,34 @@
+﻿namespace LegacyCode.FactoryMethod;
+
+internal class LegacySuperClass
+{
+    private readonly SomeClassAccessServer _someClassAccessServer;
+    private readonly SomeClassAccessDatabase _someClassAccessDatabase;
+
+    public LegacySuperClass()
+    {
+        //_someClassAccessServer = new SomeClassAccessServer();
+        //_someClassAccessDatabase = new SomeClassAccessDatabase();
+
+        _someClassAccessServer = CreateServer();
+        _someClassAccessDatabase = CreateDatabase();
+    }
+
+    protected SomeClassAccessServer CreateServer()
+        => new SomeClassAccessServer();
+
+    protected SomeClassAccessDatabase CreateDatabase()
+        => new SomeClassAccessDatabase();
+}
+
+internal class SomeClassAccessServer
+{
+    public SomeClassAccessServer()
+    {}
+}
+
+internal class SomeClassAccessDatabase
+{
+    public SomeClassAccessDatabase()
+    {}
+}
