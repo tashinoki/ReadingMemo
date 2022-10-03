aba88f5c879bfd2950fbf5b4b68f717dad27b091
tashinoki <kenya.112163@gmail.com>
2022-10-03 23:19:54 +0900

    run

--- ReadingMemo/AdaptiveCode/Main.cs
+++ ReadingMemo/AdaptiveCode/Main.cs
@@ -1,8 +1,15 @@
-﻿namespace ReadingMemo.AdaptiveCode
+﻿using System.Drawing;
+using System.Globalization;
+using ReadingMemo.AdaptiveCode.Liskov;
+
+namespace ReadingMemo.AdaptiveCode
 {
     internal static class Main
     {
         public static void Run()
-        {}
+        {
+            var shippingStrategy = new ShippingStrategy();
+            shippingStrategy.CalculateShippingCost(1000, new Size(10, 10), new RegionInfo("ja-JP"));
+        }
     }
 }
