2786b7fa274d1a50e83923394bdd6ab81db7a3b1
tashinoki <kenya.112163@gmail.com>
2022-10-04 23:52:22 +0900

    add entities

---
+++ ReadingMemo/AdaptiveCode/Liskov/Entity.cs
@@ -0,0 +1,8 @@
+﻿namespace ReadingMemo.AdaptiveCode.Liskov;
+
+internal class Entity
+{
+    public Guid Id { get; init; }
+
+    public string Name { get; private set; }
+}
---
+++ ReadingMemo/AdaptiveCode/Liskov/User.cs
@@ -0,0 +1,8 @@
+﻿namespace ReadingMemo.AdaptiveCode.Liskov;
+
+internal class User: Entity
+{
+    public Guid Id { get; init; }
+
+    public string Name { get; private set; }
+}
